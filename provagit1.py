429a90a13112f8d1da95469f22014fb878d1c011
unknown <paololocasale@gmail.com>
2020-11-18 15:39:06 +0100

    commit 4

--- provagit1.py
+++ provagit1.py
@@ -1,3 +1,4 @@
-print("primo tentativo")
-print("secondo tentativo")
-print("terzo tentativo")
+print("primo tentativo ")
+print("secondo tentativo ")
+print("terzo tentativo ")
+print("quarto tentativo ")
